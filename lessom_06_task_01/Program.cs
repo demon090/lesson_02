ccc03b1a1b5e6770d3f0b610d57c10183110b9fa
dmitriy <damrin.dima@mail.ru>
2022-12-23 11:04:32 +0500

    lesson_06

---
+++ lessom_06_task_01/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = {0, 7, 8, -2, -3};
+int result = 0;
+foreach (int number in numbers){
+    if (number > 0){
+        result++;
+    }
+}
+Console.WriteLine($"count of numbers many null: {result}" );
---
+++ lessom_06_task_01/task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("input k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"across in point ({x};{y})");
