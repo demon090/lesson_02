b9124d62c98c9503084710d2621b62f1a3834bf2
dmitriy <damrin.dima@mail.ru>
2022-12-28 11:35:43 +0500

    lesson_8

---
+++ lesson_8_task_1/task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("introduce size of array m x n and rage random of numbers:");
+int m = InputNumbers("introduce m: ");
+int n = InputNumbers("introduce n: ");
+int range = InputNumbers("introduce rage: from 1 to ");
+
+int [,] array = new int [m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+
+for (int i = 0; i < array.GetLength(0); i++){
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine){
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - string with minimal sum ({sumLine}) numbers");
+
+int SumLineElements (int [,] array, int i){
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++){
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int InputNumbers (string input){
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            array [i, j] = new Random(). Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array){
+    for (int i = 0; i < array.GetLength(1); i++){
+        for ( int j = 0; j < array.GetLength(1); j++){
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
